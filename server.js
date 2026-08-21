ae57a37f528c7a6a2bdc3944b122c7ddaae41c65
AzazhT <150243972+AzazhT@users.noreply.github.com>
2026-08-22 01:01:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 📌 የአካባቢ ተለዋዋጮች
 const TOKEN = process.env.BOT_TOKEN || '8957133551:AAGBPCGEzFLtJRXHRU0PfKJ2QXDf1AyvXec';
 const ADMIN_ID = process.env.ADMIN_ID || '686733543';
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/addis_bingo';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://robel:252@cluster0.lkrow1p.mongodb.net/?appName=Cluster0';
 const WEB_APP_URL = process.env.WEB_APP_URL || 'https://wana-bingo.onrender.com';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
